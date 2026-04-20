38b8dd7b8efcb8d58c95c543048e6e92a721f1cf
talys <talysmarques2017@gmail.com>
2026-04-20 20:47:37 -0300

    try: hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import HomePage from './pages/home'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 )
